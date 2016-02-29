595fb2bc54131e7ee7f920afa209d5b03f602763
camel <camel>
2016-02-29 21:53:19 +0100

    How to add new comment?

--- routes/articlesRoute.js
+++ routes/articlesRoute.js
@@ -5,9 +5,10 @@
 module.exports = function (app) {
     var articles = require('./../apiMethods/articlesMethods');
     app.get('/articles/comments', articles.getAllCommentedArticles);//to musi byc pierwszy API call, inaczej nie dziala ??
-    app.get('/articles/:id', articles.getSingleArticle);
+    app.get('/articles/:id/comments', articles.getSingleArticle);
     app.get('/articles', articles.getAllArticles);
     app.post('/articles', articles.addNewArticle);
     app.put('/articles/:id', articles.updateArticle);
     app.delete('/articles/:id', articles.deleteSingleArticle);
+    app.post('/dupa', articles.setArticle);
 };
